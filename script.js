4b58ff8af3d747d1537e2899f40a8f1d27a51172
IvanGomes9777 <ivanvilargomes@gmail.com>
2023-07-10 22:26:25 +0200

    save card for quiz app

---
+++ script.js
@@ -0,0 +1,10 @@
+let questions = [
+  {
+    question: "Was ist die Hauptstadt von Deutschland?",
+    answer_1: "Prag",
+    answer_2: "Nintendo",
+    answer_3: "Berlin",
+    answer_4: "Lazy Town",
+    right_answer: 3,
+  },
+];
